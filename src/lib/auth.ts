e5321d076e90909689e0fbaf11d032d2e104cec9
Ante Bucan <ab8390@g.rit.edu>
2025-06-18 14:42:34 +0200

    Added prisma file, config and created pages for login

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,11 @@
+import { PrismaClient } from "@/generated/prisma";
 import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+const prisma = new PrismaClient();
 
 export const auth = betterAuth({
+  database: prismaAdapter(prisma, { provider: "postgresql" }),
   emailAndPassword: {
     enabled: true,
     autoSignIn: false,
